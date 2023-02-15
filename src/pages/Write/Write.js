7f4a4cb9dbb8e6e83d288dcee51b65d74556783a
Sumaiya Junaid <90271730+SMYJND@users.noreply.github.com>
2023-02-15 11:20:46 +0530

    blog UI

--- src/pages/Write/Write.js
+++ src/pages/Write/Write.js
@@ -1,22 +1,71 @@
 import "./Write.css"
+import { useState } from "react";
 
 export default function Write() {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
+  const [output, setOutput] = useState([
+    {inputTitle:"1", inputContent:"===================================="},
+    {inputTitle:"2", inputContent:"===================================="},
+    {inputTitle:"3", inputContent:"===================================="},
+
+  
+  ]); 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+     setOutput([...output, {inputTitle,inputContent}]
+    );
+    setInputTitle("")
+    setInputContent("")
+  };
+
+  const handleDelete =(index) =>{
+    const removeItem = output.filter((data,i)=>{
+      return i!==index;
+    })
+    setOutput(removeItem);
+    }
+
   return (
     <div className="write">
-        <form className="writeForm">
+          <form onSubmit={handleSubmit} className="writeForm" >
             <div className="writeFormGroup">
-            <label htmlFor="fileInput" className="addFile">Add File</label>
-            <input type="file" name="" id="fileInput" style={{display:"none"}} />
-            <input type="text" placeholder="Title" className="writeInput"   />
+            <input type="text"
+              placeholder="Title"
+              className="writeInput"
+              value ={inputTitle} 
+              onChange ={(e)=>{setInputTitle(e.target.value)}}/>
             </div>
             <div className="writeFormGroup">
-                <textarea type="text" placeholder="Write here..." className="writeText" ></textarea>
+            <textarea type="text" placeholder="Write here..." className="writeText" value={inputContent} 
+            onChange = {(e) => {setInputContent(e.target.value)}}
+            ></textarea>
             </div>
-            <button className="writeSubmit">
+            <button type="submit"
+              className="btn">
                 Publish
             </button>
-
-        </form>
+            <div className="output">
+              <ul>
+              {
+              output.map((data,index)=>(
+                      <li key={index}
+                      className="writeInputList">
+                       <h1>{data.inputTitle}</h1>
+                      <p>{data.inputContent}</p>
+                      <button className="btn">Edit</button>
+                      <button className="btn" onClick={()=>{handleDelete(index)}}>Delete</button>
+                      </li>
+              ))
+            }
+              </ul>
+            </div>
+           </form>
     </div>
   )
 }
+
+
+
+
+
